b11effd408b26d77042d0c40e8cdfe1b305198f7
Sunil Kashyap <sunil@innocirc.com>
2020-11-28 12:43:55 +0530

    Updates

---
+++ precache-manifest.46bf61e13948e99b011208a07eb50118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f4f00d67e3f2b85553f25b15a29f4f",
+    "url": "/spotify_clone/index.html"
+  },
+  {
+    "revision": "fda9726d45299b6308fb",
+    "url": "/spotify_clone/static/css/main.64a3e2b2.chunk.css"
+  },
+  {
+    "revision": "564403c21e6aea8e19fb",
+    "url": "/spotify_clone/static/js/2.53db3e8d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/spotify_clone/static/js/2.53db3e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda9726d45299b6308fb",
+    "url": "/spotify_clone/static/js/main.f8f55d35.chunk.js"
+  },
+  {
+    "revision": "8101d483365e8e6752e9",
+    "url": "/spotify_clone/static/js/runtime-main.baa71473.js"
+  }
+]);
